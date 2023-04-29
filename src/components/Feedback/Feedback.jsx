19a22fe33f62bb16418e06607ab4d993c6d6a270
Anna-Mariia <122801694+bulo4ka032@users.noreply.github.com>
2023-04-29 13:19:04 +0300

    Feedback on Hooks

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,63 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions.jsx';
 import { Statistics } from '../Statistics/Statistics';
 import { NotificationMessage } from '../NotificationMessage/NotificationMessage.jsx';
 import { FeedbackForm, FbWrapper } from './Feedback.styled.jsx';
 import { Section } from 'components/Section/Section.jsx';
 
-export class Feedback extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const Feedback = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleClick = evt => {
+    switch (evt) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleClick = key => {
-    this.setState(prevState => ({
-      [key]: prevState[key] + 1,
-    }));
-  };
+  const total = good + bad + neutral;
+  const positivePercentage = +((good / total) * 100).toFixed(1);
 
-  countTotalFeedback = () => {
-    const votes = Object.values(this.state);
-    return votes.reduce((acc, vote) => acc + vote, 0);
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    if (this.state.good === 0) return '0';
-    return +((this.state.good / this.countTotalFeedback()) * 100).toFixed(1);
-  };
-
-  render() {
-    const options = Object.keys(this.state);
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-
-    return (
-      <FeedbackForm>
-        <FbWrapper>
-          <Section title="Please leave feedback" />
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.handleClick}
+  return (
+    <FeedbackForm>
+      <FbWrapper>
+        <Section title="Please leave feedback" />
+        <FeedbackOptions
+          options={{ good, neutral, bad }}
+          onLeaveFeedback={handleClick}
+        />
+      </FbWrapper>
+      <FbWrapper>
+        <Section title="Statistics" />
+        {!total ? (
+          <NotificationMessage message="There is no feedback yet" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </FbWrapper>
-        <FbWrapper>
-          <Section title="Statistics" />
-          {!total ? (
-            <NotificationMessage message="There is no feedback yet" />
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          )}
-        </FbWrapper>
-      </FeedbackForm>
-    );
-  }
-}
+        )}
+      </FbWrapper>
+    </FeedbackForm>
+  );
+};
+// }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -3,23 +3,28 @@ import PropTypes from 'prop-types';
 import { FbOptList, FbOptItem, FBOptBtn } from './FeedbackOptions.styled';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  const optionsKey = Object.keys(options);
   return (
-      <FbOptList>
-        {options.map((option) => {
-          const btnOption = option.charAt(0).toUpperCase() + option.slice(1);
-          return (
-            <FbOptItem key={option}>
-              <FBOptBtn prop={option} type='button' onClick={() => onLeaveFeedback(option)}>
-                {btnOption}
-              </FBOptBtn>
-            </FbOptItem>
-          );
-        })}
-      </FbOptList>
+    <FbOptList>
+      {optionsKey.map(option => {
+        const btnOption = option.charAt(0).toUpperCase() + option.slice(1);
+        return (
+          <FbOptItem key={option}>
+            <FBOptBtn
+              prop={option}
+              type="button"
+              onClick={() => onLeaveFeedback(option)}
+            >
+              {btnOption}
+            </FBOptBtn>
+          </FbOptItem>
+        );
+      })}
+    </FbOptList>
   );
 };
 
 FeedbackOptions.propTypes = {
-  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  options: PropTypes.objectOf(PropTypes.number).isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
-}
+};
